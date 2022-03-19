7e1019ea54d5a0faf79e36e2e8fda946b44e5ac0
long.nb <long.nb>
2022-03-19 23:05:46 +0700

    fix booking form

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,90 +3,115 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown'
 
-// import "./styles.css";
+import "./Form.css";
 
 
 export const Selector = () => {
 
-    var carCompany = ['Mazda','Suzuki','Audi'];
-    var carType = ['4 cho','7 cho','Ban tai'];
-    var carNumber = ['So san', 'Tu dong'];
-    const [value,setValue]=useState('Chon Hang xe');
-    const [value1,setValue1]=useState('Chon Loai xe');
-    const [value2,setValue2]=useState('Chon Loai hop so');
+    // var carCompany = ['Mazda','Suzuki','Audi'];
+    // var carType = ['4 cho','7 cho','Ban tai'];
+    // var carNumber = ['So san', 'Tu dong'];
+    // const [value,setValue]=useState('Chon Hang xe');
+    // const [value1,setValue1]=useState('Chon Loai xe');
+    // const [value2,setValue2]=useState('Chon Loai hop so');
 
-    const handleSelect=(e)=>{
-        for(var i = carCompany.length; i--;) {
-            if(e === carCompany[i]) {
-                console.log(e);
-                setValue(e) 
-            }
-        }
-        for(var i = carType.length; i--;) {
-            if(e === carType[i]) {
-                console.log(e);
-                setValue1(e) 
-            }
-        }
+    // const handleSelect=(e)=>{
+    //     for(var i = carCompany.length; i--;) {
+    //         if(e === carCompany[i]) {
+    //             console.log(e);
+    //             setValue(e) 
+    //         }
+    //     }
+    //     for(var i = carType.length; i--;) {
+    //         if(e === carType[i]) {
+    //             console.log(e);
+    //             setValue1(e) 
+    //         }
+    //     }
         
-        for(var i = carNumber.length; i--;) {
-            if(e === carNumber[i]) {
-                console.log(e);
-                setValue2(e) 
-            }
-        }
-    }
+    //     for(var i = carNumber.length; i--;) {
+    //         if(e === carNumber[i]) {
+    //             console.log(e);
+    //             setValue2(e) 
+    //         }
+    //     }
+    // }
 
   return (
     <>
-        
-       
-            <div className="row">
-                <div className="d-flex justify-content-center">
-                    <div className="col-md-1">
-                        <img src="https://i.pinimg.com/originals/c0/0b/69/c00b692e9820c3970e907eae9bf2be25.png" width="50" height="50"></img>
+            <div id="booking" class="section">
+                <div class="section-center">
+                    <div class="container" >
+                        <div class="row">
+                            <div class="booking-form">
+                                <div class="form-header">
+                                    <h1>Chọn xe ngay thôi</h1>
+                                </div>
+                                <form>
+                                    <div class="form-group">
+                                        <input class="form-control" type="text" placeholder="Địa chỉ"/>
+                                        <span class="form-label">Destination</span>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <select class="form-control" required>
+                                                    <option value="" selected hidden>Loại xe</option>
+                                                    <option>4 chỗ</option>
+                                                    <option>7 chỗ</option>
+                                                    <option>Bán tải</option>
+                                                </select>
+                                            <span class="select-arrow"></span>
+                                            <span class="form-label">Rooms</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <select class="form-control" required>
+                                                <option value="" selected hidden>Hãng xe</option>
+                                                <option>Audi</option>
+                                                <option>Suzuki</option>
+                                                <option>Honda</option>
+                                            </select>
+                                            <span class="select-arrow"></span>
+                                            <span class="form-label">Adults</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="form-group">
+                                            <select class="form-control" required>
+                                                <option value="" selected hidden>Hộp số</option>
+                                                <option>Số sàn</option>
+                                                <option>Số tự động</option>
+                                            </select>
+                                            <span class="select-arrow"></span>
+                                            <span class="form-label">Children</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input class="form-control" type="email" placeholder="Email"/>
+                                            <span class="form-label">Email</span>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input class="form-control" type="tel" placeholder="Số điện thoại"/>
+                                            <span class="form-label">Phone</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-btn">
+                                    <button class="submit-btn">Book Ngay</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-
-                <div className="col-md-1">
-                    <DropdownButton variant="secondary"  title={value} id="dropdown-menu-align-right" onSelect={handleSelect}>
-                        <Dropdown.Item eventKey={carCompany[0]}>{carCompany[0]}</Dropdown.Item>
-                        <Dropdown.Item eventKey={carCompany[1]}>{carCompany[1]}</Dropdown.Item>
-                        <Dropdown.Item eventKey={carCompany[2]}>{carCompany[2]}</Dropdown.Item>
-                    </DropdownButton>
                 </div>
-                
-                </div>  
             </div>
-
-     
-            <div className="row">
-                <div className="d-flex justify-content-center">
-                    <div className="col-md-1">
-                        <img src="https://i.pinimg.com/originals/c0/0b/69/c00b692e9820c3970e907eae9bf2be25.png" width="50" height="50"></img>
-                    </div>
-                    <div className="col-md-1">
-                        <DropdownButton variant="secondary"  title={value1} id="dropdown-menu-align-right" onSelect={handleSelect}>
-                            <Dropdown.Item eventKey={carType[0]}>{carType[0]}</Dropdown.Item>
-                            <Dropdown.Item eventKey={carType[1]}>{carType[1]}</Dropdown.Item>
-                            <Dropdown.Item eventKey={carType[2]}>{carType[2]}</Dropdown.Item>
-                        </DropdownButton>
-                    </div>
-                </div>   
-            </div>
-
-            <div className="row">
-                <div className="d-flex justify-content-center">
-                    <div className="col-md-1">
-                        <img src="https://i.pinimg.com/originals/c0/0b/69/c00b692e9820c3970e907eae9bf2be25.png" width="50" height="50"></img>
-                    </div>
-                    <div className="col-md-1">
-                        <DropdownButton variant="secondary"  title={value2} id="dropdown-menu-align-right" onSelect={handleSelect}>
-                            <Dropdown.Item eventKey={carNumber[0]}>{carNumber[0]}</Dropdown.Item>
-                            <Dropdown.Item eventKey={carNumber[1]}>{carNumber[1]}</Dropdown.Item>
-                        </DropdownButton>
-                    </div>
-                </div>   
-            </div>
+        </div>
        
     </>
   );
